1cc89faf720c7e16d2d15b7b521dc5ded64ed74f
seralekseenko <seroleksiienko@gmail.com>
2024-04-22 16:49:24 +0300

    resolve test for: non_marginable_buying_power, equity + improve tests

--- tests/test_account_info.py
+++ tests/test_account_info.py
@@ -1,26 +1,42 @@
 import unittest
 import random
 from unittest.mock import patch
-from aic_tools_alpaca.account_info import CheckIfAccountRestricted
-from aic_tools_alpaca.account_info import GetBuyingPower
+from aic_tools_alpaca.account_info import (
+    CheckIfTradingBlocked, GetNonMarginableBuyingPower, GetTotalBuyingPower, GetAccountEquity
+)
 
 
 class TestAccountFunctions(unittest.TestCase):
+    @staticmethod
+    def _set_mock_acc_attribute(mock_acc, attribute_name, value):
+        setattr(mock_acc, attribute_name, value)
+
+    def _test_account_buying_powers(self, mock_acc, account_info_class, attribute_name):
+        random_value = random.randint(0, 999999)
+        self._set_mock_acc_attribute(mock_acc, attribute_name, random_value)
+        self.assertEqual(account_info_class()._run(), f"{random_value}$")
+
     @patch('aic_tools_alpaca.account_info.acc')
-    def test_check_account_status_blocked(self, mock_acc):
-        mock_acc.trading_blocked = True
-        self.assertTrue(CheckIfAccountRestricted()._run())
-    
+    def test_check_trading_blocked_true(self, mock_acc):
+        self._set_mock_acc_attribute(mock_acc, 'trading_blocked', True)
+        self.assertTrue(CheckIfTradingBlocked()._run())
+
     @patch('aic_tools_alpaca.account_info.acc')
-    def test_check_account_status_not_blocked(self, mock_acc):
-        mock_acc.trading_blocked = False
-        self.assertFalse(CheckIfAccountRestricted()._run())
-        
+    def test_check_trading_not_blocked_false(self, mock_acc):
+        self._set_mock_acc_attribute(mock_acc, 'trading_blocked', False)
+        self.assertFalse(CheckIfTradingBlocked()._run())
+
     @patch('aic_tools_alpaca.account_info.acc')
-    def test_print_buying_power(self, mock_acc):
-        random_buying_power = random.randint(10000, 200000)
-        mock_acc.buying_power = random_buying_power
-        self.assertEqual(GetBuyingPower()._run(), f"{random_buying_power}$")
+    def test_total_buying_power(self, mock_acc):
+        self._test_account_buying_powers(mock_acc, GetTotalBuyingPower, 'buying_power')
+
+    @patch('aic_tools_alpaca.account_info.acc')
+    def test_non_margin_buying_power(self, mock_acc):
+        self._test_account_buying_powers(mock_acc, GetNonMarginableBuyingPower, 'non_marginable_buying_power')
+
+    @patch('aic_tools_alpaca.account_info.acc')
+    def test_account_equity(self, mock_acc):
+        self._test_account_buying_powers(mock_acc, GetAccountEquity, 'equity')
 
 
 if __name__ == '__main__':
