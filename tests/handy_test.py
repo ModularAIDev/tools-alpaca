05185c816a4fb70e3ab6ad9d10e33d988878aba2
seralekseenko <seroleksiienko@gmail.com>
2024-05-06 17:27:20 +0300

    add env secrets cleaning

--- tests/handy_test.py
+++ tests/handy_test.py
@@ -1,3 +1,4 @@
+import os
 from aic_tools_alpaca.account_info import (
     CheckIfTradingBlocked, GetNonMarginableBuyingPower, GetTotalBuyingPower, GetAccountEquity
 )
@@ -6,3 +7,14 @@ print(f"Is trading prohibited: {CheckIfTradingBlocked()._run()}")
 print(f"Accountant total purchasing power is: {GetTotalBuyingPower()._run()}")
 print(f"Accountant purchasing power without margin is: {GetNonMarginableBuyingPower()._run()}")
 print(f"Total equinty is: {GetAccountEquity()._run()}")
+
+
+def clear_env(keys):
+    """Remove specified ENVs."""
+    for key in keys:
+        os.environ.pop(key, None)  # is pop safety removing?
+        print(f"ENV removed for '{key}': {key not in os.environ}")
+
+
+keys_to_clear = ['API_KEY_ID', 'SECRET_KEY']
+clear_env(keys_to_clear)
